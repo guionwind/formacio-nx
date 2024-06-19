7db1b35c521547040e599a7cf50342298d689a74
Sheng Ye <95938918+guionwind@users.noreply.github.com>
2024-06-19 11:04:56 +0200

    lab4(wip): pending header style (check executors maybe?)

--- bg-hoard/store/src/app/app.component.ts
+++ bg-hoard/store/src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { getAllGames } from '../fake-api';
 import { MatCardModule } from '@angular/material/card';
+import { HeaderComponent } from '@bg-hoard/store/ui-shared';
 @Component({
   standalone: true,
   selector: 'bg-hoard-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [MatCardModule, CommonModule],
+  imports: [MatCardModule, CommonModule, HeaderComponent],
 })
 export class AppComponent {
   title = 'Board Game Hoard';
